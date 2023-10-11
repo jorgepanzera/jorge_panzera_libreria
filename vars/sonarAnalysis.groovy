51ffbd7826fd531c629731988a201e67b1f84d00
Jorge <panzera.jorge@gmail.com>
2023-10-11 20:40:06 -0300

    Ejercicio 3

---
+++ vars/sonarAnalysis.groovy
@@ -0,0 +1,17 @@
+// projectKey - nombre del proyecto a analizar
+// abortPipeline - Define si aborta o continua el pipeline si falla el scanner
+def call(projectKey, abortPipeline = false) {
+    def scannerResult = 1
+
+    timeout(time: 5, unit: 'MINUTES') {
+        withSonarQubeEnv(installationName: 'SonarLocal', credentialsId: credentialsId)  {
+            scannerResult = bat(script: "sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.sources=.", returnStatus: true)
+        }
+    }
+
+    if (abortPipeline && scannerResult != 0) {
+        error("SonarQube scan failed with result code: ${scannerResult}")
+    }
+
+    return scannerResult
+}
