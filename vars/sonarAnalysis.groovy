bda93020ba04f81a848e4c875e40be9293b2eb96
Jorge <panzera.jorge@gmail.com>
2023-10-13 20:43:50 -0300

    modify sonarAnalysys

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,19 +1,25 @@
 def call(projectKey, gitBranch, abortPipeline = false) {
     def scannerResult = 1
+    def haveToExitPipeline = false
 
-    bat "echo Mi rama en la libreria es ${gitBranch}"
-    bat 'echo Shared Library: %BRANCH_NAME%'
-
-    timeout(time: 5, unit: 'MINUTES') {
+    timeout(time: 30, unit: 'SECONDS') {
         withSonarQubeEnv(installationName: 'SonarLocal', credentialsId: 'SonarQube_Token')  {
             scannerResult = bat(script: "sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.sources=.", returnStatus: true)
         }
     }
 
     if (abortPipeline && scannerResult != 0) {
+        haveToExitPipeline = true
+    } else if (!abortPipeline) {
+        // Verificar si abortar el pipeline segun nombre de la rama gitBranch
+        if (gitBranch == 'master' || gitBranch.startsWith('hotfix')) {
+            haveToExitPipeline = true
+        }
+    }
+
+    if (haveToExitPipeline) {
         error("SonarQube scan failed with result code: ${scannerResult}")
     }
 
     return scannerResult
-
 }
